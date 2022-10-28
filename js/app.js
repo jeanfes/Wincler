22d887b0a11fc9a9ccee06fb2fcfbf19f193adf8
JEANFES <jeanescobar7@hotmail.com>
2022-10-27 22:02:09 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function classToggle() {
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('step-animation');
+  }
+  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
